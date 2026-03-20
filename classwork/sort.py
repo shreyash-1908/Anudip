e738d805c56fcdf6413d2d4a80424c44760cfa42
Yuvraj Patel <uvrajpatel2004@gmail.com>
2026-03-20 09:57:02 +0530

    Descending order

--- classwork/sort.py
+++ classwork/sort.py
@@ -3,18 +3,18 @@ def sort(arr):
     n = len(arr)
 
     for i in range(n):
-        min_index = i
+        max_index = i  
 
         for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
+            if arr[j] > arr[max_index]: 
+                max_index = j
 
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        arr[i], arr[max_index] = arr[max_index], arr[i]
 
     return arr
 
 
-arr=[64, 25, 12, 22, 11,15,26,3,7,23]
+arr = [64, 25, 12, 22, 11, 15, 26, 3, 7, 23]
 print("Original array:", arr)
 sorted_arr = sort(arr)
 print("Sorted array:", sorted_arr)
